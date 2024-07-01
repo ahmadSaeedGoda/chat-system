c80a5d738c48435add61e19ce4e649b546418896
ahmadsaeedgoda <ahmad.saeed.goda@gmail.com>
2024-07-01 11:26:02 +0300

    Refactor `MessagesTestSuite` and add more test cases
    - Introduce a new package for centralized responses.
    
    - Utilize the newly introduced responses structure in `MessagesTestSuite`.
    
    - Authenticate requests hitting messages protected endpoints across tests.
    
    - Replace routers by `httptest` package for recording responses.
    
    - Use middlewares for testing their functionality along with requests and to avoid tests panic on errors.
    
    - Add more tests for corner cases of sending a new message.
    
    - Add test cases for fetching messages.
    
    Fixes: #a9ecc92

--- internal/api/handlers/messages_test.go
+++ internal/api/handlers/messages_test.go
@@ -2,34 +2,34 @@ package handlers
 
 import (
 	"bytes"
+	"chat-system/internal/api/auth"
 	common "chat-system/internal/api/common/constants"
+	"chat-system/internal/api/common/responses"
 	"chat-system/internal/api/middlewares"
 	"chat-system/internal/models"
 	"chat-system/mocks"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 
-	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
 )
 
-type ErrRes struct {
-	Error string `json:"error"`
-}
-
 type MessagesTestSuite struct {
 	suite.Suite
-	handler                MsgHandler
-	msgService             *mocks.MessageService
-	userService            *mocks.UserService
-	server                 *httptest.Server
-	reqContentType         string
-	sendEndpointReqPath    string
-	getMsgsEndpointReqPath string
-	errResponse            *ErrRes
+	msgService         *mocks.MessageService
+	userService        *mocks.UserService
+	sendEndpointUrl    string
+	getMsgsEndpointUrl string
+	authHeader         string
+	handler            *msgHandler
+	errResponse        *responses.ErrResponse
+	middleware         func(handlerMethod func(w http.ResponseWriter, r *http.Request)) http.Handler
 }
 
 func TestMessagesTestSuite(t *testing.T) {
@@ -37,28 +37,35 @@ func TestMessagesTestSuite(t *testing.T) {
 }
 
 func (mts *MessagesTestSuite) SetupSuite() {
-	mts.reqContentType = "application/json"
-	mts.sendEndpointReqPath = "/send"
-	mts.getMsgsEndpointReqPath = "/messages"
-	mts.errResponse = &ErrRes{}
-}
-
-func (mts *MessagesTestSuite) SetupTest() {
-	r := mux.NewRouter()
-	r.Use(middlewares.HandleErrors)
+	// Set up environment variable
+	os.Setenv("AUTH_HEADER_PREFIX", "Bearer")
+	os.Setenv("JWT_SECRET_KEY", "secret")
 
 	mts.msgService = &mocks.MessageService{}
 	mts.userService = &mocks.UserService{}
+
+	reqSenderUsername := "User1"
+	token, err := auth.GenerateToken(reqSenderUsername)
+	mts.NoError(err, "Failed to create token")
+
+	mts.authHeader = fmt.Sprintf("Bearer %s", token)
+
 	mts.handler = NewMsgHandler(mts.msgService, mts.userService)
 
-	r.HandleFunc("/send", mts.handler.SendMessage).Methods("POST")
-	r.HandleFunc("/messages", mts.handler.GetMessages).Methods("GET")
+	mts.middleware = func(handlerMethod func(w http.ResponseWriter, r *http.Request)) http.Handler {
+		return middlewares.IsAuth(
+			http.HandlerFunc(
+				middlewares.HandleErrors(
+					http.HandlerFunc(handlerMethod),
+				).ServeHTTP,
+			),
+		)
+	}
 
-	mts.server = httptest.NewServer(r)
-}
+	mts.sendEndpointUrl = "localhost/send"
+	mts.getMsgsEndpointUrl = "localhost/api/v1/messages"
 
-func (mts *MessagesTestSuite) TearDownTest() {
-	mts.server.Close()
+	mts.errResponse = &responses.ErrResponse{}
 }
 
 func (mts *MessagesTestSuite) Test_Send_Invalid_Input_NoRecipient() {
@@ -66,9 +73,17 @@ func (mts *MessagesTestSuite) Test_Send_Invalid_Input_NoRecipient() {
 	invalidBody := &models.SendMessageInput{Content: content}
 	body, err := json.Marshal(invalidBody)
 	mts.NoError(err, "Failed to marshal registerInput")
-	expectedErr := errors.New(common.BAD_REQUEST)
-	resp, err := http.Post(mts.server.URL+mts.sendEndpointReqPath, mts.reqContentType, bytes.NewBuffer(body))
+
+	req, err := http.NewRequest("POST", mts.sendEndpointUrl, bytes.NewBuffer(body))
 	mts.NoError(err, "Failed to make POST request")
+
+	req.Header.Set("Authorization", mts.authHeader)
+
+	rr := httptest.NewRecorder()
+
+	mts.middleware(mts.handler.SendMessage).ServeHTTP(rr, req)
+
+	resp := rr.Result()
 
 	mts.Equal(http.StatusBadRequest, resp.StatusCode)
 
@@ -76,7 +91,6 @@ func (mts *MessagesTestSuite) Test_Send_Invalid_Input_NoRecipient() {
 	mts.NoError(err, "Failed to decode response body")
 
 	mts.Equal(common.BAD_REQUEST, mts.errResponse.Error)
-	mts.Equal(expectedErr.Error(), mts.errResponse.Error)
 }
 
 func (mts *MessagesTestSuite) Test_Send_Invalid_Input_NoContent() {
@@ -85,9 +99,16 @@ func (mts *MessagesTestSuite) Test_Send_Invalid_Input_NoContent() {
 	body, err := json.Marshal(invalidBody)
 	mts.NoError(err, "Failed to marshal registerInput")
 
-	expectedErr := errors.New(common.BAD_REQUEST)
-	resp, err := http.Post(mts.server.URL+mts.sendEndpointReqPath, mts.reqContentType, bytes.NewBuffer(body))
+	req, err := http.NewRequest("POST", mts.sendEndpointUrl, bytes.NewBuffer(body))
 	mts.NoError(err, "Failed to make POST request")
+
+	req.Header.Set("Authorization", mts.authHeader)
+
+	rr := httptest.NewRecorder()
+
+	mts.middleware(mts.handler.SendMessage).ServeHTTP(rr, req)
+
+	resp := rr.Result()
 
 	mts.Equal(http.StatusBadRequest, resp.StatusCode)
 
@@ -95,5 +116,187 @@ func (mts *MessagesTestSuite) Test_Send_Invalid_Input_NoContent() {
 	mts.NoError(err, "Failed to decode response body")
 
 	mts.Equal(common.BAD_REQUEST, mts.errResponse.Error)
+}
+
+func (mts *MessagesTestSuite) Test_Send_Err_Checking_Recipient() {
+	expectedErr := errors.New(common.INTERNAL_SERVER_ERROR)
+	mts.userService.On("UserExists", mock.Anything).Return(false, expectedErr).Once()
+
+	recipient := "x"
+	content := "Test"
+	reqBody := &models.SendMessageInput{Recipient: recipient, Content: content}
+	body, err := json.Marshal(reqBody)
+	mts.NoError(err, "Failed to marshal registerInput")
+
+	req, err := http.NewRequest("POST", mts.sendEndpointUrl, bytes.NewBuffer(body))
+	mts.NoError(err, "Failed to make POST request")
+
+	req.Header.Set("Authorization", mts.authHeader)
+
+	rr := httptest.NewRecorder()
+
+	mts.middleware(mts.handler.SendMessage).ServeHTTP(rr, req)
+
+	resp := rr.Result()
+
+	mts.Equal(http.StatusInternalServerError, resp.StatusCode)
+
+	err = json.NewDecoder(resp.Body).Decode(&mts.errResponse)
+	mts.NoError(err, "Failed to decode response body")
+
+	mts.Equal(common.INTERNAL_SERVER_ERROR, mts.errResponse.Error)
+}
+
+func (mts *MessagesTestSuite) Test_Send_Recipient_Not_Found() {
+	mts.userService.On("UserExists", mock.Anything).Return(false, nil).Once()
+
+	recipient := "x"
+	content := "Test"
+	reqBody := &models.SendMessageInput{Recipient: recipient, Content: content}
+	body, err := json.Marshal(reqBody)
+	mts.NoError(err, "Failed to marshal registerInput")
+
+	req, err := http.NewRequest("POST", mts.sendEndpointUrl, bytes.NewBuffer(body))
+	mts.NoError(err, "Failed to make POST request")
+
+	req.Header.Set("Authorization", mts.authHeader)
+
+	rr := httptest.NewRecorder()
+
+	mts.middleware(mts.handler.SendMessage).ServeHTTP(rr, req)
+
+	resp := rr.Result()
+
+	mts.Equal(http.StatusBadRequest, resp.StatusCode)
+
+	err = json.NewDecoder(resp.Body).Decode(&mts.errResponse)
+	mts.NoError(err, "Failed to decode response body")
+
+	mts.Equal(common.SEND_MESSAGE_NO_RECIPIENT, mts.errResponse.Error)
+}
+
+func (mts *MessagesTestSuite) Test_Send_DB_Err() {
+	mts.userService.On("UserExists", mock.Anything).Return(true, nil).Once()
+
+	expectedErr := errors.New(common.INTERNAL_SERVER_ERROR)
+	mts.msgService.On("CreateMessage", mock.Anything).Return(expectedErr).Once()
+
+	recipient := "x"
+	content := "Test"
+	reqBody := &models.SendMessageInput{Recipient: recipient, Content: content}
+	body, err := json.Marshal(reqBody)
+	mts.NoError(err, "Failed to marshal registerInput")
+
+	req, err := http.NewRequest("POST", mts.sendEndpointUrl, bytes.NewBuffer(body))
+	mts.NoError(err, "Failed to make POST request")
+
+	req.Header.Set("Authorization", mts.authHeader)
+
+	rr := httptest.NewRecorder()
+
+	mts.middleware(mts.handler.SendMessage).ServeHTTP(rr, req)
+
+	resp := rr.Result()
+
+	mts.Equal(http.StatusInternalServerError, resp.StatusCode)
+
+	err = json.NewDecoder(resp.Body).Decode(&mts.errResponse)
+	mts.NoError(err, "Failed to decode response body")
+
+	mts.Equal(common.INTERNAL_SERVER_ERROR, mts.errResponse.Error)
 	mts.Equal(expectedErr.Error(), mts.errResponse.Error)
+}
+
+func (mts *MessagesTestSuite) Test_Send_Success() {
+	recipient := "User2"
+	content := "Test Content"
+	reqBody := &models.SendMessageInput{Recipient: recipient, Content: content}
+	body, err := json.Marshal(reqBody)
+	mts.NoError(err, "Failed to marshal registerInput")
+
+	req, err := http.NewRequest("POST", mts.sendEndpointUrl, bytes.NewBuffer(body))
+	mts.NoError(err, "Failed to make POST request")
+
+	req.Header.Set("Authorization", mts.authHeader)
+
+	mts.userService.On("UserExists", mock.Anything).Return(true, nil).Once()
+	mts.msgService.On("CreateMessage", mock.Anything).Return(nil).Once()
+	mts.msgService.On("UpdateCachedMsgsForUser", mock.Anything, mock.Anything).Return(nil).Twice()
+
+	rr := httptest.NewRecorder()
+
+	mts.middleware(mts.handler.SendMessage).ServeHTTP(rr, req)
+
+	resp := rr.Result()
+
+	mts.Equal(http.StatusCreated, resp.StatusCode)
+
+	msgResponse := &responses.MessageResponse{}
+	err = json.NewDecoder(resp.Body).Decode(&msgResponse)
+	mts.NoError(err, "Failed to decode response body")
+
+	mts.Equal(msgResponse.Recipient, recipient)
+	mts.Equal(msgResponse.Content, content)
+	mts.Equal(msgResponse.Sender, "User1")
+}
+
+func (mts *MessagesTestSuite) Test_GetMessages_Success() {
+	req, err := http.NewRequest("GET", mts.getMsgsEndpointUrl, nil)
+	mts.NoError(err, "Failed to make request")
+
+	req.Header.Set("Authorization", mts.authHeader)
+
+	mts.msgService.On("GetFromCache", mock.Anything).Return(nil, nil).Once()
+
+	expectedMsg := models.Message{Sender: "Mickey", Recipient: "Minnie", Content: "Hi"}
+	msgsArr := make([]models.Message, 0)
+	msgsArr = append(msgsArr, expectedMsg)
+	mts.msgService.On("GetMessages", mock.Anything).Return(msgsArr, nil).Once()
+
+	mts.msgService.On("SetMessagesToCache", mock.Anything, mock.Anything).Return(nil).Once()
+
+	rr := httptest.NewRecorder()
+
+	mts.middleware(mts.handler.GetMessages).ServeHTTP(rr, req)
+
+	resp := rr.Result()
+
+	mts.Equal(http.StatusOK, resp.StatusCode)
+
+	var msgsResponse responses.MessagesResponse
+	err = json.NewDecoder(resp.Body).Decode(&msgsResponse)
+	mts.NoError(err, "Failed to decode response body")
+
+	mts.Len(msgsResponse.Messages, 1)
+	mts.Equal(msgsResponse.Messages[0].Sender, expectedMsg.Sender)
+	mts.Equal(msgsResponse.Messages[0].Recipient, expectedMsg.Recipient)
+	mts.Equal(msgsResponse.Messages[0].Content, expectedMsg.Content)
+}
+
+func (mts *MessagesTestSuite) Test_GetMessages_Error() {
+	req, err := http.NewRequest("GET", mts.getMsgsEndpointUrl, nil)
+	mts.NoError(err, "Failed to make request")
+
+	req.Header.Set("Authorization", mts.authHeader)
+
+	mts.msgService.On("GetFromCache", mock.Anything).Return(nil, nil).Once()
+
+	expectedErr := errors.New("DB is Down :(")
+	mts.msgService.On("GetMessages", mock.Anything).Return(nil, expectedErr).Once()
+
+	mts.msgService.On("SetMessagesToCache", mock.Anything, mock.Anything).Return(nil).Once()
+
+	rr := httptest.NewRecorder()
+
+	mts.middleware(mts.handler.GetMessages).ServeHTTP(rr, req)
+
+	resp := rr.Result()
+
+	mts.Equal(http.StatusInternalServerError, resp.StatusCode)
+
+	var errResponse responses.ErrResponse
+	err = json.NewDecoder(resp.Body).Decode(&errResponse)
+	mts.NoError(err, "Failed to decode response body")
+
+	mts.Equal(errResponse.Error, common.INTERNAL_SERVER_ERROR)
 }
